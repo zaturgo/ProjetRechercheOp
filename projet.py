36da272c4c230c802698af2a7247d9a308ede66b
Te-nam <pubmjpub@free.fr>
2019-03-11 18:54:32 +0100

    Lecture des zones couvertes par les capteurs

--- projet.py
+++ projet.py
@@ -1,1 +1,23 @@
 print("coucou")
+
+nombre_capteurs = 0
+nombre_zones = 0
+capteurs_zones_duree_vie = []
+erreur = False
+
+with open('test', 'r') as fichier_a_lire:
+    nombre_capteurs = fichier_a_lire.readline().strip()
+    nombre_zones = fichier_a_lire.readline().strip()
+    duree_vie_capteur = fichier_a_lire.readline().split()
+
+    if (len(duree_vie_capteur) != int(nombre_capteurs)):
+        erreur = True
+    else:
+        for i in range(0, int(nombre_capteurs)):
+            ligne = list(map(int, fichier_a_lire.readline().split()))
+            ligne.sort()
+            print(ligne)
+
+
+if not erreur:
+    print("Ok !")
